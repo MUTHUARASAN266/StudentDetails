fa28406adb054f638c0347ae81396a277d6efeb6
Mutharasan karuppaiyan <muthuarasan@colanonline.com>
2024-07-25 14:17:47 +0530

    Initial commit

---
+++ app/src/main/java/com/studentdetails/DemoScreen.kt
@@ -0,0 +1,22 @@
+package com.studentdetails
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.studentdetails.databinding.ActivitySplashBinding
+
+class DemoScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnLoginScreen.setOnClickListener {
+                startActivity(Intent(this@DemoScreen,LoginScreen::class.java))
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/studentdetails/LoginScreen.kt
@@ -0,0 +1,17 @@
+package com.studentdetails
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.studentdetails.databinding.ActivityLoginScreenBinding
+
+class LoginScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
